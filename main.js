a8d7a040a991eab6d8e394469a833bd40a92183c
Andreas <andreas.brandenborg@live.dk>
2024-10-10 14:52:13 +0200

    added a whole bunch

---
+++ main.js
@@ -0,0 +1,21 @@
+let darkmode = localStorage.getItem("darkmode")
+
+const themeSwitch = document.getElementById("theme-switch")
+
+const enableDarkmode = () => {
+    document.body.classList.add("darkmode")
+    localStorage.setItem("darkmode", "active")
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove("darkmode")
+    localStorage.setItem("darkmode", null)
+}
+
+themeSwitch.addEventListener("click", () => {
+    if (darkmode !== "active") {
+        enableDarkmode()
+    } else {
+        disableDarkmode()
+    }
+})
